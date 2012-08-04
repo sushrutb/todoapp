08e4cd2d481c79b140923d190d3b7cd445b80213
sushrut <sushrut@ubuntu.ubuntu-domain>
2012-08-04 19:19:41 +0530

    changed settings

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -9,6 +9,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
