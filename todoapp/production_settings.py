81ae6b8d18f42c589fe7fb0787ec618bdc8b19c5
sushrut <sushrut@ubuntu.ubuntu-domain>
2012-08-18 16:58:20 +0530

    minor changes.

--- todoapp/production_settings.py
+++ todoapp/production_settings.py
@@ -16,7 +16,7 @@ SITE_DOMAIN = 'todoapp.sushrutbidwai.com'
 STATIC_URL = 'https://s3.amazonaws.com/prod.todoapp.sushrutbidwai.com/'
 AWS_STORAGE_BUCKET_NAME = 'prod.todoapp.sushrutbidwai.com'
 
-STATIC_URL = '//s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME + 'docs/assets/'
+STATIC_URL = '//s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
